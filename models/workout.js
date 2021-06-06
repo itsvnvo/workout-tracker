30ccaba1ed82b2cac6919a7f227f0374e680382d
Vincent Vo <itsvnvo@gmail.com>
2021-06-06 01:40:48 -0500

    intial commit, added model workout.js

---
+++ models/workout.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [
+        {
+            name: {
+                type: String,
+                trim: true,
+            },
+            type: {
+                type: String,
+                trim: true,
+            },
+            weight: {
+                type: Number,
+                default: 0
+            },
+            sets: {
+                type: Number,
+                default: 0
+            },
+            reps: {
+                type: Number,
+                default: 0
+            },
+            duration: Number,
+            distance: {
+                type: Number,
+                default: 0
+            }
+        }
+    ],
+});
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
